90db0ce0d29fbde56eb180c46477adc150bd1e1f
Szymon Kozioł <szymonkoz@student.agh.edu.pl>
2023-01-09 18:28:28 +0100

    Created login and register page

--- src/client/target/login.ts
+++ src/client/target/login.ts
@@ -1,3 +1,3 @@
-import '../css/styles.css';
-import '../css/login.css';
+import '$css/styles.css';
+import '$css/authPage.css';
 export {};
--- src/client/target/register.ts
+++ src/client/target/register.ts
@@ -1,3 +1,3 @@
-import '../css/styles.css';
-import '../css/register.css';
+import '$css/styles.css';
+import '$css/authPage.css';
 export {};
